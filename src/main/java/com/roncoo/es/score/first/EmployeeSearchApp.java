83ba9fb37c9f04a02ed81a1b06a1cdd6cd3b181b
zgl <361251205@qq.com>
2019-08-15 00:33:07 +0800

    new

--- src/main/java/com/roncoo/es/score/first/EmployeeSearchApp.java
+++ src/main/java/com/roncoo/es/score/first/EmployeeSearchApp.java
@@ -1,5 +1,7 @@
 package com.roncoo.es.score.first;
 
+
+import io.netty.handler.codec.json.JsonObjectDecoder;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchResponse;
@@ -13,10 +15,13 @@ import org.elasticsearch.index.query.GeoDistanceQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.index.store.Store;
+import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
-
+import  org.elasticsearch.common.geo.*;
 import java.net.InetAddress;
+import java.util.ArrayList;
 import java.util.List;
+
 
 
 public class EmployeeSearchApp {
@@ -31,6 +36,19 @@ public class EmployeeSearchApp {
 //        executeSearch(client);
         createjinweidu(client);
         getStores(client);
+        double da=121.423177d;
+        double xiao=31.239423d;
+        for (int i = 0; i <1000 ; i++) {
+            xiao=xiao+ 0.00001 ;
+            da = da + 0.0001d;
+            createjinweidufor(client, xiao  + 0.001+"", da + 0.0001d+"");
+
+        }
+        long startTime=System.currentTimeMillis();            //获得当前时间
+        storeRange(client);
+        long endTime=System.currentTimeMillis();                //获得当前时间
+        System.out.println(endTime-startTime);
+
     }
 
      private static void executeSearch(TransportClient client) {
@@ -117,14 +135,28 @@ public class EmployeeSearchApp {
          IndexResponse response = client.prepareIndex("geo", "city", "1")
                  .setSource(XContentFactory.jsonBuilder()
                          .startObject()
-                         .field("name", "上海")
-                         .field("location", "31.226789,121.495775")
+                         .field("name", "南京")
+                         .field("location", "31.240041,121.423249")
                          .endObject())
                  .get();
          System.out.println(response.getResult());
-
-
      }
+
+//121.423249,31.240041
+    /**
+     * 创建经纬度 for （创建一个document）
+     * @param client
+     */
+    private static void createjinweidufor(TransportClient client ,String xiao,String da) throws Exception {
+        IndexResponse response = client.prepareIndex("geo", "city")
+                .setSource(XContentFactory.jsonBuilder()
+                        .startObject()
+                        .field("name", "南京")
+                        .field("location", xiao+","+da)
+                        .endObject())
+                .get();
+        System.out.println(response.getResult());
+    }
 
 
      /**
@@ -152,5 +184,60 @@ public class EmployeeSearchApp {
 
          return null ;
      }
+
+
+    public static void storeRange(TransportClient client) {
+
+        //坐标集合
+        List<GeoPoint> geoPoints = new ArrayList<GeoPoint>();
+        GeoPoint geoPoint = new GeoPoint();
+        geoPoint.reset(31.239516, 121.41944);
+        geoPoints.add(geoPoint);
+
+        GeoPoint geoPoint2 = new GeoPoint();
+        geoPoint2.reset(31.236706, 121.42986);
+        geoPoints.add(geoPoint2);
+
+        GeoPoint geoPoint3 = new GeoPoint();
+        geoPoint3.reset(31.24566, 121.424111);
+        geoPoints.add(geoPoint3);
+
+
+        //拼接查询条件
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+        QueryBuilder locationBuilder = QueryBuilders.geoPolygonQuery("location", geoPoints);
+        QueryBuilder isdeleteBuilder = QueryBuilders.matchAllQuery();
+        boolQueryBuilder.filter(locationBuilder);
+        boolQueryBuilder.must(isdeleteBuilder);
+        client.prepareSearch("geo").setTypes("city")
+                .setQuery(boolQueryBuilder)
+                .setFrom(0)
+                .setSize(10).get()
+        ;
+
+
+//        JsonObject jsonObject = result.getJsonObject();
+//        JsonObject hitsobject = jsonObject.getAsJsonObject("hits");
+//        long took = jsonObject.get("took").getAsLong();
+//        long total = hitsobject.get("total").getAsLong();
+//        JsonArray jsonArray = hitsobject.getAsJsonArray("hits");
+
+//        System.out.println("hi:======" +client.prepareSearch("geo").setTypes("city")
+//                .setQuery(boolQueryBuilder)
+//                .setFrom(0)
+//                .setSize(10).get().getHits().getHits());
+
+        SearchHit[] hits = client.prepareSearch("geo").setTypes("city")
+                .setQuery(boolQueryBuilder)
+                .setFrom(0)
+                .setSize(10).get().getHits().getHits();
+
+        for (SearchHit hit : hits) {
+            String sourceAsString = hit.getSourceAsString();
+            System.out.println(sourceAsString);
+        }
+
+
+    }
 
 }
